50d2e2290e3c82da5008563ddbb7d93c9a5d6583
Eugenio Cano-Manuel Mendoza <eugeniocanom@fastmail.com>
2025-03-16 14:04:40 +0000

    Don't consume AST as we process it

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -20,62 +20,72 @@ impl Interpreter {
         match prog {
             Program::Declarations(decls) => {
                 for decl in decls {
-                    self.execute(decl);
+                    self.execute(&decl);
                 }
             }
         }
     }
 
-    pub fn execute(&mut self, decl: Declaration) {
-        match decl {
-            Declaration::Statement(Stmt::ExprStmt(expr)) => {
+    fn execute_stmt(&mut self, stmt: &Stmt) {
+        match stmt {
+            Stmt::ExprStmt(expr) => {
                 let _ = eval(&expr, &mut self.environment);
             }
-            Declaration::Statement(Stmt::IfStmt {
+            Stmt::IfStmt {
                 condition,
                 then_branch,
                 else_branch,
-            }) => {
+            } => {
                 let condition_value = match eval(&condition, &mut self.environment) {
                     crate::syntax::Literal::Boolean(value) => value,
                     literal => panic!("if condition can only be boolean, got '{literal:?}'"),
                 };
                 if condition_value {
-                    self.execute(Declaration::Statement(*then_branch));
+                    self.execute_stmt(then_branch);
                 } else {
                     else_branch
-                        .map(|else_branch| self.execute(Declaration::Statement(*else_branch)));
+                        .as_ref()
+                        .map(|else_branch| self.execute_stmt(else_branch));
                 }
             }
-            Declaration::Statement(Stmt::WhileStmt { condition, body }) => loop {
+            Stmt::PrintStmt(expr) => {
+                let value = eval(&expr, &mut self.environment);
+                println!("{value}")
+            }
+            Stmt::WhileStmt { condition, body } => loop {
                 let condition_value = match eval(&condition, &mut self.environment) {
                     crate::syntax::Literal::Boolean(value) => value,
                     literal => panic!("while condition can only be boolean, got '{literal:?}'"),
                 };
                 if condition_value {
-                    self.execute(Declaration::Statement(*body.clone()));
+                    self.execute_stmt(body);
                 } else {
                     break;
                 }
             },
-            Declaration::Statement(Stmt::PrintStmt(expr)) => {
-                let value = eval(&expr, &mut self.environment);
-                println!("{value}")
-            }
-            Declaration::Statement(Stmt::Block(decls)) => {
+            Stmt::Block(decls) => {
                 self.execute_block(decls);
+            }
+        }
+    }
+
+    pub fn execute(&mut self, decl: &Declaration) {
+        match decl {
+            Declaration::Statement(stmt) => {
+                self.execute_stmt(stmt);
             }
             Declaration::Var {
                 identifier,
                 expression,
             } => {
                 let value = eval(&expression, &mut self.environment);
-                self.environment.define(Identifier(identifier), value);
+                self.environment
+                    .define(Identifier(identifier.clone()), value);
             }
         }
     }
 
-    fn execute_block(&mut self, statements: Vec<Declaration>) {
+    fn execute_block(&mut self, statements: &[Declaration]) {
         self.environment.push_env();
 
         for stmt in statements {
